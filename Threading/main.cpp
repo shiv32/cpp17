bb3d26b7c9ba7c51f6119c3fe9d42caab75b0787
shiv <shiv.xylem@gmail.com>
2024-02-19 22:18:40 -0800

    shared_future

--- Threading/main.cpp
+++ Threading/main.cpp
@@ -1,29 +1,51 @@
-// Exception Handling std::async
+// std::shared_future
 // g++ --std=c++17 main.cpp -o test -pthread
 
 #include <iostream>
 #include <future>
 
-int calculate()
-{
-    throw std::runtime_error("Exception thrown from calculate().");
-}
-
 int main()
 {
-    // Use the launch::async policy to force asynchronous execution.
-    auto myFuture = std::async(std::launch::async, calculate);
-    // Do some more work...
-    // Get the result.
-    try
+    std::promise<void> thread1Started, thread2Started;
+
+    std::promise<int> signalPromise;
+
+    auto signalFuture = signalPromise.get_future().share();
+    //shared_future<int> signalFuture(signalPromise.get_future());
+
+    auto function1 = [&thread1Started, signalFuture]
     {
-        int result = myFuture.get();
-        std::cout << result << std::endl;
-    }
-    catch (const std::exception &ex)
+        thread1Started.set_value();
+        // Wait until parameter is set.
+        int parameter = signalFuture.get();
+
+        std::cout<<"parameter 1 : "<<parameter<<std::endl;
+        
+    };
+    auto function2 = [&thread2Started, signalFuture]
     {
-        std::cout << "Caught exception: " << ex.what() << std::endl;
-    }
+        thread2Started.set_value();
+
+        // Wait until parameter is set.
+        int parameter = signalFuture.get();
+
+        std::cout<<"parameter 2 : "<<parameter<<std::endl;
+        
+    };
+
+    // Run both lambda expressions asynchronously.
+    // Remember to capture the future returned by async()!
+    auto result1 = std::async(std::launch::async, function1);
+    auto result2 = std::async(std::launch::async, function2);
+
+    // Wait until both threads have started.
+    thread1Started.get_future().wait();
+    thread2Started.get_future().wait();
+
+    // Both threads are now waiting for the parameter.
+    // Set the parameter to wake up both of them.
+    signalPromise.set_value(42);
+
 
     return 0;
 }
---
+++ Threading/main_18.cpp
@@ -0,0 +1,29 @@
+// Exception Handling std::async
+// g++ --std=c++17 main.cpp -o test -pthread
+
+#include <iostream>
+#include <future>
+
+int calculate()
+{
+    throw std::runtime_error("Exception thrown from calculate().");
+}
+
+int main()
+{
+    // Use the launch::async policy to force asynchronous execution.
+    auto myFuture = std::async(std::launch::async, calculate);
+    // Do some more work...
+    // Get the result.
+    try
+    {
+        int result = myFuture.get();
+        std::cout << result << std::endl;
+    }
+    catch (const std::exception &ex)
+    {
+        std::cout << "Caught exception: " << ex.what() << std::endl;
+    }
+
+    return 0;
+}
