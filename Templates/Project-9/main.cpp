8cf06db86d57a082b191f98a7864a970b4006e64
shiv <yadav.shivkumar5@gmail.com>
2024-03-05 23:27:48 +0530

    Limit Class Template Instantiations

---
+++ Templates/Project-9/main.cpp
@@ -0,0 +1,17 @@
+/*
+Limit Class Template Instantiations
+
+g++ -std=c++17 main.cpp -o test
+*/
+
+#include <iostream>
+#include "grid.hpp"
+
+int main()
+{
+
+    Grid<int> myIntGrid; 
+    myIntGrid.at(0, 0) = 10;
+
+    return 0;
+}
