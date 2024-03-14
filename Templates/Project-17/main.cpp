3a7d32b4d329da2a372449a1bc5b9768d80e7a4e
shiv <yadav.shivkumar5@gmail.com>
2024-03-14 22:14:29 +0530

    Deriving from Class Templates

--- Templates/Project-17/main.cpp
+++ Templates/Project-17/main.cpp
@@ -10,11 +10,21 @@ g++ -std=c++17 main.cpp chesspiece.cpp -o test
 
 int main()
 {
-
     GameBoard<ChessPiece> chessBoard(8, 8);
     ChessPiece pawn;
     chessBoard.at(0, 0) = pawn;
     chessBoard.move(0, 0, 0, 1);
+
+    //----------------------------------------------------------------
+    GameBoard<int> myInt(8, 8);
+    myInt.at(0, 0) = 2;
+
+    std::cout << "(0,0) : "<< myInt.at(0, 0).value_or(0) << std::endl;
+
+    myInt.move(0, 0, 0, 1);
+
+    std::cout << "(0,0) : "<< myInt.at(0, 0).value_or(0) << std::endl;
+    std::cout << "(0,1) : "<< myInt.at(0, 1).value_or(0) << std::endl;
 
     return 0;
 }
