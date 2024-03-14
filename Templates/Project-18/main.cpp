211fbef63835ef8b3f331d1f8e278324c19a5ed0
shiv <yadav.shivkumar5@gmail.com>
2024-03-14 22:36:05 +0530

    Alias Templates

---
+++ Templates/Project-18/main.cpp
@@ -0,0 +1,46 @@
+/*
+Alias Templates
+
+g++ *.cpp -o test
+*/
+
+#include <iostream>
+
+template <typename T1, typename T2>
+class MyTemplateClass
+{
+public:
+    explicit MyTemplateClass(T1 width, T2 height) : mWidth{width}, mHeight{height}
+    {
+        std::cout << "mWidth : " << mWidth << std::endl;
+        std::cout << "mHeight : " << mHeight << std::endl;
+    }
+
+    ~MyTemplateClass() = default;
+
+private:
+    T1 mWidth{};
+    T2 mHeight{};
+};
+
+//---------- Alias Templates -------------------
+using MyInt = int;
+
+using OtherName = MyTemplateClass<int, double>;
+
+template <typename T1>
+using OtherName2 = MyTemplateClass<T1, double>;
+
+int main()
+{
+    MyInt val = 5;
+    std::cout << "MyInt val : " << val << std::endl;
+
+    std::cout << "--------- ob1 ------------" << std::endl;
+    OtherName ob1(3, 5.7);
+
+    std::cout << "--------- ob2 ------------" << std::endl;
+    OtherName2<double> ob2(3.3, 7.8);
+
+    return 0;
+}
