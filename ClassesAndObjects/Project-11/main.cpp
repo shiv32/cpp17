7bdb40d3fc590dced7622de4f642caf976c0c3f4
shiv32 <yadav.shivkumar5@gmail.com>
2024-04-23 20:53:16 +0530

    Constructors on the Heap

---
+++ ClassesAndObjects/Project-11/main.cpp
@@ -0,0 +1,39 @@
+/*
+
+Constructors on the Heap
+
+g++ -std=c++17 *.cpp -o test
+
+*/
+
+#include <iostream>
+#include <memory>
+#include "SpreadsheetCell.hpp"
+
+int main()
+{
+    auto smartCellp = std::make_unique<SpreadsheetCell>(40);
+
+    // ... do something with the cell, no need to delete the smart pointer
+    std::cout << "smartCellp cell 1: " << smartCellp->getValue() << std::endl;
+
+
+    // Or with raw pointers, without smart pointers (not recommended)
+    SpreadsheetCell *myCellp = new SpreadsheetCell(50);
+
+    std::cout << "myCellp cell 1: " << myCellp->getValue() << std::endl;
+    
+    SpreadsheetCell *anotherCellp = nullptr;
+    anotherCellp = new SpreadsheetCell(40);
+
+    // ... do something with the cells
+    std::cout << "anotherCellp cell 1: " << anotherCellp->getValue() << std::endl;
+   
+    delete myCellp;
+    myCellp = nullptr;
+   
+    delete anotherCellp;
+    anotherCellp = nullptr;
+
+    return 0;
+}
