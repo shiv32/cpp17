4dac60c4016d993523ed19822c5c9042034cb87e
shiv32 <yadav.shivkumar5@gmail.com>
2024-04-23 20:28:54 +0530

    object creation

---
+++ ClassesAndObjects/Project-8/MyClass.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string>
+
+class MyClass
+{
+private:
+    std::string mName;
+};
---
+++ ClassesAndObjects/Project-8/main.cpp
@@ -0,0 +1,19 @@
+/*
+
+Object creation
+When an object is created, all its embedded objects are also created.
+Whenever an object is created, one of its constructors is executed.
+C++ programmers sometimes call a constructor a ctor
+
+g++ -std=c++17 *.cpp -o test
+
+*/
+
+#include <iostream>
+#include "MyClass.hpp"
+
+int main()
+{
+    MyClass obj;
+    return 0;
+}
