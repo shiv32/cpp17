cb9f4a040587e7c11210f0c5c1fdbb676ea1255e
shiv32 <yadav.shivkumar5@gmail.com>
2024-04-23 20:38:11 +0530

    Writing Constructors

---
+++ ClassesAndObjects/Project-9/main.cpp
@@ -0,0 +1,23 @@
+/*
+
+Writing Constructors
+
+g++ -std=c++17 *.cpp -o test
+
+*/
+
+#include <iostream>
+#include <memory>
+#include "SpreadsheetCell.hpp"
+
+int main()
+{
+    //------------------ Using Smart Pointer --------------------------------------------------
+    auto myCellp3 = std::make_unique<SpreadsheetCell>(3.7);
+    // Equivalent to:
+    // std::unique_ptr<SpreadsheetCell> myCellp3(new SpreadsheetCell());
+
+    std::cout << "cell 1: " << myCellp3->getValue() << " " << myCellp3->getString() << std::endl;
+
+    return 0;
+}
