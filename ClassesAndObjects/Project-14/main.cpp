df2ec32f3ab080a227c791c4fbce83a315e629f6
shiv <yadav.shivkumar5@gmail.com>
2024-05-01 21:26:11 +0530

    Compiler-Generated Default Constructor

---
+++ ClassesAndObjects/Project-14/main.cpp
@@ -0,0 +1,21 @@
+/*
+
+Compiler-Generated Default Constructor
+
+g++ -std=c++17 *.cpp -o test
+
+*/
+
+#include <iostream>
+#include <memory>
+#include "SpreadsheetCell.hpp"
+
+int main()
+{
+    SpreadsheetCell myCell;
+    myCell.setValue(16);
+
+    std::cout << "cell 1 : " << myCell.getValue() << std::endl;
+
+    return 0;
+}
