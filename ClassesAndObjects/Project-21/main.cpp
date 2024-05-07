3bb2146ffa405b912db6ad2e618d003064893749
shiv <yadav.shivkumar5@gmail.com>
2024-05-07 20:49:16 +0530

    Explicitly Defaulted and Deleted Copy Constructor

---
+++ ClassesAndObjects/Project-21/main.cpp
@@ -0,0 +1,18 @@
+/*
+Explicitly Defaulted and Deleted Copy Constructor
+
+g++ -std=c++17 *.cpp -o test
+*/
+
+#include <iostream>
+#include "SpreadsheetCell.hpp"
+
+int main()
+{
+    SpreadsheetCell myCell(61.8);
+    SpreadsheetCell myCell2(myCell); // myCell2 has the same values as myCell
+
+    std::cout << myCell2.getValue() << std::endl;
+
+    return 0;
+}
