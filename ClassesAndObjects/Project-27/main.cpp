663ea13e5b5e16157da569d98f7aa0fc4c15c204
shiv <yadav.shivkumar5@gmail.com>
2024-05-30 23:13:02 +0530

    Objects as Return Values

---
+++ ClassesAndObjects/Project-27/main.cpp
@@ -0,0 +1,24 @@
+/*
+    Objects as Return Values
+
+    g++ -std=c++17 *.cpp -o test
+*/
+
+#include <iostream>
+#include "SpreadsheetCell.hpp"
+
+int main()
+{
+    SpreadsheetCell myCell2(5);
+    std::string s1;
+    s1 = myCell2.getString(); // invokes the copy constructor and the assignment operator (for two different objects).
+                              // Compilers are free and sometimes required to implement Return Value Optimization (RVO) (copy elision)
+                              // to optimize away costly copy constructions when returning values.
+
+    SpreadsheetCell myCell3(5);
+    std::string s2 = myCell3.getString(); // s2 gets its copy constructor called, not its assignment operator.
+                                          // With move semantics, the compiler can use a move constructor instead of a copy constructor to
+                                          // return the string from getString(). This is more efficient.
+
+    return 0;
+}
