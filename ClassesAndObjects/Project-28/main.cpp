29206970db1163543919693f3d410cebe8ddba1b
shiv <yadav.shivkumar5@gmail.com>
2024-05-31 23:25:26 +0530

    Copy Constructors and Object Members

---
+++ ClassesAndObjects/Project-28/main.cpp
@@ -0,0 +1,19 @@
+/*
+   Copy Constructors and Object Members
+   
+    g++ -std=c++17 *.cpp -o test
+*/
+
+#include <iostream>
+#include "SpreadsheetCell.hpp"
+
+int main()
+{
+    SpreadsheetCell myCell(5);
+
+    // Constructed with the copy constructor, because this is a declaration.
+    // The operator= is not called for this line!
+    SpreadsheetCell anotherCell(myCell);
+
+    return 0;
+}
