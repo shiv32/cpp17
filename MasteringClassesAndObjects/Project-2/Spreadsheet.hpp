78d96c1b209712f03f310292eb879d55b00f8b32
shiv <shiv.xylem@gmail.com>
2024-06-18 02:39:37 -0700

    Freeing Memory with Destructors update

--- MasteringClassesAndObjects/Project-2/Spreadsheet.hpp
+++ MasteringClassesAndObjects/Project-2/Spreadsheet.hpp
@@ -10,10 +10,10 @@ public:
     ~Spreadsheet();
     void setCellAt(size_t x, size_t y, const SpreadsheetCell &cell);
     SpreadsheetCell &getCellAt(size_t x, size_t y);
-    void verifyCoordinate(size_t x, size_t y) const;
 
 private:
     bool inRange(size_t value, size_t upper) const;
+    void verifyCoordinate(size_t x, size_t y) const;
     size_t mWidth = 0;
     size_t mHeight = 0;
     SpreadsheetCell **mCells = nullptr;
