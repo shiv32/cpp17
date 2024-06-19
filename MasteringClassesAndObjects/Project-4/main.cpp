bbdb1d188c10d02c5bc69eb9d0faf2778d04f9db
shiv <shiv.xylem@gmail.com>
2024-06-19 04:28:39 -0700

    Rvalue References

--- MasteringClassesAndObjects/Project-4/main.cpp
+++ MasteringClassesAndObjects/Project-4/main.cpp
@@ -7,10 +7,22 @@
 */
 
 #include <iostream>
+#include <string>
+
+// lvalue reference parameter
+void handleMessage(std::string &message)
+{
+    std::cout << "handleMessage with lvalue reference: " << message << std::endl;
+}
+// rvalue reference parameter
+void handleMessage(std::string &&message)
+{
+    std::cout << "handleMessage with rvalue reference: " << message << std::endl;
+}
 
 int main()
 {
-
+    system("clear && printf '\e[3J'"); // clean the terminal before output in linux
     /*
      a is an lvalue, it has a name and you can take the address of it with &a.
      4 * 2 on the other hand is an rvalue.
@@ -22,9 +34,14 @@ int main()
 
     /*
     An rvalue reference is a reference to an rvalue
-    A function can specify an rvalue reference parameter by using && as part of 
+    A function can specify an rvalue reference parameter by using && as part of
     the parameter specification; for example, type&& name.
     */
+
+    std::string b = "Hello ";
+    std::string c = "World";
+
+    handleMessage(b); // Calls handleMessage(string& value)
 
     return 0;
 }
