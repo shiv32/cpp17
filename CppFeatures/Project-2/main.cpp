2126b4e5ef6254694fed41b9cac086185f446a03
shiv <yadav.shivkumar5@gmail.com>
2024-07-09 00:01:43 +0530

    assert

--- CppFeatures/Project-2/main.cpp
+++ CppFeatures/Project-2/main.cpp
@@ -1,7 +1,20 @@
+/*
+        The assert macro in C++ is used to verify assumptions made by the program 
+        and to catch bugs during the development phase.
+*/
+
 #include <iostream>
+#include <cassert>
 
-int main() {
-	
+int main()
+{
+    system("clear && printf '\e[3J'"); // clean the terminal before output in linux
 
-	return 0;
+    int x = 5;
+
+    // assert(x == 5); // This will pass
+
+    assert(x == 0); // This will fail and terminate the program
+
+    return 0;
 }
