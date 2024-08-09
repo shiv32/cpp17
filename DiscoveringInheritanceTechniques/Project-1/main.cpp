67bd552aa9ca31e0c8009dcb01db641266f0ff54
shiv <shiv.xylem@gmail.com>
2024-08-09 03:51:38 -0700

    Extending Classes

---
+++ DiscoveringInheritanceTechniques/Project-1/main.cpp
@@ -0,0 +1,37 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * 
+ * @brief BUILDING CLASSES WITH INHERITANCE
+ *          Extending Classes
+ *              When you write a class definition in C++, you can tell the compiler that your class is 
+ *              inheriting from, deriving from, or extending an existing class. 
+
+                By doing so, your class automatically contains the data members and methods of the original class,
+                which is called the parent class or base class or superclass. 
+
+                Extending an existing class gives your class (which is now called a derived class or a sub-
+                class) the ability to describe only the ways in which it is different from the parent class.
+
+                To show the syntax for inheritance, two classes are used, called Base and Derived.
+
+ 
+ * @version 0.1
+ * @date 2024-08-09
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+   
+
+#include <iostream>
+
+int main()
+{
+
+    system("clear && printf '\e[3J'"); // clean the terminal before output in linux
+
+    
+
+    return 0;
+}
