edd74a1da90a807fc35fcc834119e13b8096433d
shiv <shiv.xylem@gmail.com>
2024-08-16 04:11:45 -0700

    Hiding Instead of Overriding

---
+++ DiscoveringInheritanceTechniques/Project-4/Derived.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file Derived.cpp
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2024-08-11
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include "Derived.hpp"
+
+ void Derived::someMethod(double d) 
+//void Derived::someMethod(int i) 
+{
+  std::cout << __PRETTY_FUNCTION__ << std::endl;
+}
+
+void Derived::go() {  std::cout << __PRETTY_FUNCTION__ << std::endl; }
+
+void Derived::someOtherMethod() 
+{
+  std::cout << __PRETTY_FUNCTION__ << std::endl;
+
+  std::cout << "I can access base class data member mProtectedInt."
+            << std::endl;
+  std::cout << "Its value is " << mProtectedInt << std::endl;
+  // std::cout << "The value of mPrivateInt is " << mPrivateInt << std::endl; //
+  // Error!
+}
---
+++ DiscoveringInheritanceTechniques/Project-4/DerivedDerived.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file DerivedDerived.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-08-11
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#include "DerivedDerived.hpp"
+
+void DerivedDerived::someOtherMethod()
+{
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+}
