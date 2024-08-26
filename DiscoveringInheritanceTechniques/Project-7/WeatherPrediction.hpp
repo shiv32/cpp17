63e2a85b744add763cda575befa1993ad9ffbbd0
shiv <shiv.xylem@gmail.com>
2024-08-26 02:26:44 -0700

    WeatherPrediction

---
+++ DiscoveringInheritanceTechniques/Project-7/WeatherPrediction.hpp
@@ -0,0 +1,10 @@
+/**
+ * @file WeatherPrediction.hpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-08-26
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
---
+++ DiscoveringInheritanceTechniques/Project-7/main.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-08-26
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#include <iostream>
+
+int main()
+{
+    system("clear && printf '\e[3J'"); // clean the terminal before output in linux
+    return 0;
+}
