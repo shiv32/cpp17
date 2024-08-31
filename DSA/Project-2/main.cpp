9c438663669f4ae04fd7b36e83554788ed23fe8b
shiv <yadav.shivkumar5@gmail.com>
2024-08-31 13:57:36 +0530

    Pointers

--- DSA/Project-2/main.cpp
+++ DSA/Project-2/main.cpp
@@ -2,6 +2,16 @@
  * @file main.cpp
  * @author your name (you@domain.com)
  * @brief Pointers
+ * 
+                (Process)
+
+                ⬇   stack <- local variables
+                ⬆   heap  <- dynamically allocated memory
+                    data  <- global and static variables
+                    code  <- programs executable code
+
+            (virtual address space)
+
  * @version 0.1
  * @date 2024-08-31
  * 
@@ -15,8 +25,11 @@ int main()
 {
     system("clear && printf '\e[3J'"); // clean the terminal before output in linux
 
-    int data = 30;
-    int *ptr = &data;
+    int data = 30;      //stack
+    int *ptr = &data;  //stack
+
+    ptr = new int(7); //heap
+    delete ptr;
 
     return 0;
 }
