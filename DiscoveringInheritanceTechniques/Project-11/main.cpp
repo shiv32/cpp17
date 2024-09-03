79529f6375bdfe3b4a6a8bbb06dd6d8ea8171a55
shiv <shiv.xylem@gmail.com>
2024-09-02 23:04:49 -0700

    Referring to Parent Names

---
+++ DiscoveringInheritanceTechniques/Project-11/main.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief   Referring to Parent Names part 2
+ * @version 0.1
+ * @date 2024-09-02
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include <iostream>
+
+int main()
+{
+    system("clear && printf '\e[3J'"); // clean the terminal before output in linux
+
+    return 0;
+}
