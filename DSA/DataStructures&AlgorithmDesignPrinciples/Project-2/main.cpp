b93666019ec745e4dec20ca2a9297038b32c13fd
shiv <yadav.shivkumar5@gmail.com>
2024-09-03 23:30:57 +0530

    ds

--- DSA/DataStructures&AlgorithmDesignPrinciples/Project-2/main.cpp
+++ DSA/DataStructures&AlgorithmDesignPrinciples/Project-2/main.cpp
@@ -17,16 +17,59 @@ int main()
 {
     system("clear && printf '\e[3J'"); // clean the terminal before output in linux
 
-    int nStudents{1};
-    
-    // std::cout << "Enter number of students in class 1: ";
-    // std::cin >> nStudents;
+    int nStudents{};
+    std::cout << "Enter number of students in class 1 : ";
+    std::cin >> nStudents;
 
     dynamic_array<student> class1(nStudents);
 
+    for (int i = 0; i < nStudents; i++)
+    {
+        std::cout << "Enter name and class of student " << i + 1 << ": ";
+        std::string name;
+        int standard;
+        std::cin >> name >> standard;
+        class1[i] = student{name, standard};
+    }
+
+    // Now, let’s try to access the student out of range in the array
+    try
+    {
+        //std::cout << "class1 student set out of range without exception" << std::endl;
+
+        //class1[nStudents] = student{"John", 8}; // No exception, undefined behavior
+
+        class1.at(nStudents) = student{"John", 8}; // Will throw exception
+    }
+    catch (...)
+    {
+        std::cout << "Exception caught" << std::endl;
+    }
+
     auto class2 = class1; // Deep copy
 
+    std::cout << "Second class after initialized using first array : " << class2.to_string() << std::endl;
+
     auto class3 = class1 + class2; // Combines both classes and creates a bigger one
+
+    std::cout << "Combined class : ";
+    std::cout << class3.to_string() << std::endl;
+
+    return 0;
+
+    //---------------------------------------------------------------------------------------
+    // system("clear && printf '\e[3J'"); // clean the terminal before output in linux
+
+    // int nStudents{1};
+
+    // std::cout << "Enter number of students in class 1: ";
+    // std::cin >> nStudents;
+
+    // dynamic_array<student> class1(nStudents);
+
+    // auto class2 = class1; // Deep copy
+
+    // auto class3 = class1 + class2; // Combines both classes and creates a bigger one
 
     // for (int i = 0; i < nStudents; i++)
     // {
@@ -53,14 +96,14 @@ int main()
     //     std::cout << "Exception caught" << std::endl;
     // }
 
-   //auto class2 = class1; // Deep copy
+    // auto class2 = class1; // Deep copy
 
     // std::cout << "Second class after initialized using first array: " << class2.to_string() << std::endl;
 
-   // auto class3 = class1 + class2; // Combines both classes and creates a bigger one
+    // auto class3 = class1 + class2; // Combines both classes and creates a bigger one
 
     // std::cout << "Combined class: ";
     // std::cout << class3.to_string() << std::endl;
 
-    return 0;
+    // return 0;
 }
