e32201c973634db7070909ec5a8cff07996cd535
shiv <shiv.xylem@gmail.com>
2024-09-23 03:07:06 -0700

    INHERITANCE FOR POLYMORPHISM

--- DiscoveringInheritanceTechniques/Project-13/main.cpp
+++ DiscoveringInheritanceTechniques/Project-13/main.cpp
@@ -3,10 +3,10 @@
  * @author your name (you@domain.com)
  * @brief   INHERITANCE FOR POLYMORPHISM
                 Now that you understand the relationship between a derived class and its parent.
-                You can use inheritance in its most powerful scenario—polymorphism. 
-            
+                You can use inheritance in its most powerful scenario—polymorphism.
+
             Return of the Spreadsheet
-                A SpreadsheetCell represents a single element of data. 
+                A SpreadsheetCell represents a single element of data.
                 Up to now, that element always stored a single double value.
 
                 Note that a cell can be set either as a double or a string, but it is always stored as a double.
@@ -23,26 +23,26 @@
                                             static double stringToDouble(std::string_view inString);
                                             double mValue;
                                     };
-                
+
                 In a real spreadsheet application, cells can store different things.
                 A cell could store a double, but it might just as well store a piece of text.
-                There could also be a need for additional types of cells, such as a formula cell, or a date cell. 
+                There could also be a need for additional types of cells, such as a formula cell, or a date cell.
 
             Designing the Polymorphic Spreadsheet Cell
 
- 
+
 
  * @version 0.1
  * @date 2024-09-23
- * 
+ *
  * @copyright Copyright (c) 2024
- * 
+ *
  */
 
 #include <iostream>
 
 int main()
 {
-
+    system("clear && printf '\e[3J'"); // clean the terminal before output in linux
     return 0;
 }
