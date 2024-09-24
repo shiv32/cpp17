ce68ef8a49caf08c4b6de3cc9f4869865cc5e9fc
shiv <yadav.shivkumar5@gmail.com>
2024-09-24 23:57:36 +0530

    File IO

--- CppFeatures/FileInputOutput/main.cpp
+++ CppFeatures/FileInputOutput/main.cpp
@@ -16,6 +16,35 @@
 #include <iostream>
 #include <string>
 #include <experimental/filesystem>
+#include <fstream>
+#include <string>
+
+void Write()
+{
+    //std::ofstream out{"data.txt"}; //open file using ctor
+    //or
+    std::ofstream out;
+    out.open("data.txt");  //open file using open fun
+
+    out << "Hello world" << std::endl;
+    out << 20 << std::endl;
+    out.close();
+}
+
+void Read()
+{
+    // std::ifstream input{"data.txt"}; //open file using ctor
+    //or
+    std::ifstream input;
+    input.open("data.txt"); //open file using open fun
+
+    std::string message;
+    std::getline(input, message);
+    int value{};
+    input >> value;
+    input.close();
+    std::cout << message << " : " << value << std::endl;
+}
 
 int main()
 {
@@ -35,35 +64,37 @@ int main()
      std::cout << message << std::endl;
      */
 
-    //-------------------------(Introduction to filesystem library)----------------------------------
-    using namespace std::experimental::filesystem;
-    path p{R"(/tmp/test.txt)"};
+    //-------------------------(Introduction to filesystem library (C++17) (Deprecated))----------------------------------
+    // using namespace std::experimental::filesystem;
+    // path p{R"(/tmp/test.txt)"};
 
-    if (p.has_filename())
-    {
-        std::cout << p.filename() << std::endl;
-    }
+    // if (p.has_filename())
+    // {
+    //     std::cout << p.filename() << std::endl;
+    // }
 
-    std::cout << "------" << std::endl;
+    // std::cout << "------" << std::endl;
 
-    for (const auto &x : p)
-    {
-        std::cout << x << std::endl;
-    }
+    // for (const auto &x : p)
+    // {
+    //     std::cout << x << std::endl;
+    // }
 
-    std::cout << "------" << std::endl;
+    // std::cout << "------" << std::endl;
 
-     path p2{R"(/tmp)"};
+    //  path p2{R"(/tmp)"};
 
-    directory_iterator beg{p2};
-    directory_iterator end{};
+    // directory_iterator beg{p2};
+    // directory_iterator end{};
 
-    while (beg != end)
-    {
-        std::cout << *beg << std::endl;
-        ++beg;
-    }
-    //---------------------------------------()---------------------------------------------------
+    // while (beg != end)
+    // {
+    //     std::cout << *beg << std::endl;
+    //     ++beg;
+    // }
+    //---------------------------------------(File IO part-1 basics)---------------------------------------------------
+    Write();
+    Read();
 
     return 0;
 }
