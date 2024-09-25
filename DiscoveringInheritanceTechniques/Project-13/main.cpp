00dfafb10254c6d52911e29bbf65653221c46bcf
shiv <shiv.xylem@gmail.com>
2024-09-25 04:16:21 -0700

    INHERITANCE FOR POLYMORPHISM

--- DiscoveringInheritanceTechniques/Project-13/main.cpp
+++ DiscoveringInheritanceTechniques/Project-13/main.cpp
@@ -29,7 +29,40 @@
                 There could also be a need for additional types of cells, such as a formula cell, or a date cell.
 
             Designing the Polymorphic Spreadsheet Cell
+                Narrow the scope of the SpreadsheetCell to cover only strings, 
+                perhaps renaming it StringSpreadsheetCell in the process. 
+                To handle doubles, a second class, DoubleSpreadsheetCell, would inherit from the StringSpreadsheetCell and
+                provide functionality specific to its own format.
 
+                            StringSpreadsheetCell <--- DoubleSpreadsheetCell
+
+                The derived class would override most, if not all, of the functionality of the base class. 
+                Because doubles are treated differently from strings in almost all cases. 
+                There clearly is a relationship between a cell containing strings and a cell containing doubles.
+                Which implies that somehow a DoubleSpreadsheetCell “is-a” StringSpreadsheetCell.
+
+                A better design would make these classes peers with a common parent, SpreadsheetCell.
+
+                                
+                                        /---DoubleSpreadsheetCell
+                            SpreadsheetCell
+                                        \---StringSpreadsheetCell
+
+                It shows a polymorphic approach to the SpreadsheetCell hierarchy.
+                Because DoubleSpreadsheetCell and StringSpreadsheetCell both inherit from a common 
+                parent, SpreadsheetCell, they are interchangeable in the view of other code. 
+
+                 In practical terms, that means the following:
+
+                    ➤ Both derived classes support the same interface (set of methods) defined by the base class.
+
+                    ➤ Code that makes use of SpreadsheetCell objects can call any method in the interface without 
+                    even knowing whether the cell is a DoubleSpreadsheetCell or a StringSpreadsheetCell.
+                    
+                    ➤ Through the magic of virtual methods, the appropriate instance of every method in the
+                    interface is called depending on the class of the object.
+                    
+                    ➤ Other data structures, can contain a collection of multi-typed cells by referring to the parent type.
 
 
  * @version 0.1
