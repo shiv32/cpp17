88d6a1a85b894696c4309575dd296146aca1568e
shiv <shiv.xylem@gmail.com>
2024-10-06 21:46:47 -0700

    polymorphism

---
+++ DiscoveringInheritanceTechniques/Project-15/main.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief  The Individual Derived Classes
+                Writing the StringSpreadsheetCell and DoubleSpreadsheetCell classes is just a matter of
+                implementing the functionality that is defined in the parent. 
+
+                If a derived class does not implement all pure virtual methods from the base class, then 
+                the derived class is abstract as well, and clients will not be able to instantiate objects of
+                the derived class.
+               
+
+                    
+
+             
+              
+
+ * @version 0.1
+ * @date 2024-09-23
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include <iostream>
+
+int main()
+{
+    system("clear && printf '\e[3J'"); // clean the terminal before output in linux
+    return 0;
+}
