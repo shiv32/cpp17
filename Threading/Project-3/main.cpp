f9fd5c95a55124f17aeb9c7168a560b590d6aa33
shiv <yadav.shivkumar5@gmail.com>
2024-10-08 22:16:02 +0530

    cc

---
+++ Threading/Project-3/main.cpp
@@ -0,0 +1,37 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief
+ *
+            Thread with Lambda
+
+            g++ main.cpp -o test -pthread && ./test && rm test
+ *
+ * @version 0.1
+ * @date 2024-10-08
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include <iostream>
+#include <thread>
+
+int main()
+{
+    system("clear && printf '\e[3J'"); // clean the terminal before output in linux
+
+    int id = 1;
+    int numIterations = 5;
+
+    std::thread t1([id, numIterations]
+                   {
+                            for (int i = 0; i < numIterations; ++i) {
+                            std::cout << "Counter " << id << " has value " << i << std::endl;
+                            } 
+                    }
+
+                  );
+
+    t1.join();
+}
---
+++ Threading/Project-4/main.cpp
@@ -0,0 +1,44 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief
+ *
+ *
+            Thread with Member Function
+
+            g++ main.cpp -o test -pthread && ./test && rm test
+ *
+ *
+ * @version 0.1
+ * @date 2024-10-08
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include <iostream>
+#include <thread>
+
+class Request
+{
+public:
+    Request(int id) : mId(id) {}
+    void process()
+    {
+        std::cout << "Processing request " << mId << std::endl;
+    }
+
+private:
+    int mId;
+};
+
+int main()
+{
+    system("clear && printf '\e[3J'"); // clean the terminal before output in linux
+
+    Request req(100);
+
+    std::thread t{&Request::process, &req};
+
+    t.join();
+}
