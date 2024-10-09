ec5121898e9543e907fbc75014645ca8778a7f1e
shiv <shiv.xylem@gmail.com>
2024-10-09 09:46:52 -0700

    Inheritance

---
+++ DiscoveringInheritanceTechniques/Project-16/main.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief   MULTIPLE INHERITANCE
+                Multiple inheritance is often perceived as a complicated and unnecessary
+                part of object-oriented programming.
+                This section explains the mechanics of multiple inheritance in C++.
+
+
+
+ * @version 0.1
+ * @date 2024-09-23
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include <iostream>
+
+int main()
+{
+    system("clear && printf '\e[3J'"); // clean the terminal before output in linux
+    
+
+    return 0;
+}
