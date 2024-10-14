e05e6672816c5cb911c6748e0691500ee68a8765
shiv <yadav.shivkumar5@gmail.com>
2024-10-15 01:02:31 +0530

    lambda expression

--- CppFeatures/LambdaExpressionsC++11/main.cpp
+++ CppFeatures/LambdaExpressionsC++11/main.cpp
@@ -16,21 +16,67 @@
 
 #include <iostream>
 
+// template <typename T, int size>
+// void Sort(T (&arr)[size])
+// {
+//     for (int i = 0; i < size - 1; ++i)
+//     {
+//         for (int j = 0; j < size - 1; ++j)
+//         {
+//             if (arr[j] > arr[j + 1])
+//             {
+//                 T temp = arr[j];
+//                 arr[j] = arr[j + 1];
+//                 arr[j + 1] = temp;
+//             }
+//         }
+//     }
+// }
+
+// template <typename T, int size>
+// void Sort(T (&arr)[size])
+// {
+//     for (int i = 0; i < size - 1; ++i)
+//     {
+//         for (int j = 0; j < size - 1; ++j)
+//         {
+//             if (arr[j] > arr[j + 1])
+//             {
+//                 T temp = std::move(arr[j]);
+//                 arr[j] = std::move(arr[j + 1]);
+//                 arr[j + 1] = std::move(temp);
+//             }
+//         }
+//     }
+// }
+
+using Comparator = bool (*)(int, int);
+
 template <typename T, int size>
-void Sort(T (&arr)[size])
+void Sort(T (&arr)[size], Comparator comp)
 {
     for (int i = 0; i < size - 1; ++i)
     {
         for (int j = 0; j < size - 1; ++j)
         {
-            if (arr[j] > arr[j + 1])
+            if (comp(arr[j], arr[j + 1]))
             {
-                T temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+                T temp = std::move(arr[j]);
+                arr[j] = std::move(arr[j + 1]);
+                arr[j + 1] = std::move(temp);
             }
         }
     }
+}
+
+bool CompAsc(int x, int y)
+{
+    return x > y;
+}
+
+bool CompDes(int x, int y)
+{
+    return x < y;
 }
 
 int main()
@@ -47,7 +93,9 @@ int main()
 
     std::cout << std::endl;
 
-    Sort(arr);
+    // Sort(arr);
+    // Sort(arr,CompAsc);
+    Sort(arr, CompDes);
 
     for (auto x : arr)
     {
