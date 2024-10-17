3b0fb7dfed37f808b9e2d84d0eefcfdbb85e2fd6
shiv <yadav.shivkumar5@gmail.com>
2024-10-18 00:39:09 +0530

    benchmark

--- CppFeatures/Project-3/main.cpp
+++ CppFeatures/Project-3/main.cpp
@@ -1,7 +1,7 @@
 /**
  * @file main.cpp
  * @author your name (you@domain.com)
- * @brief inline function vs normal function
+ * @brief benchmark inline function vs normal function
  * @version 0.1
  * @date 2024-10-16
  *
@@ -9,6 +9,108 @@
  *
  */
 
+// #include <iostream>
+// #include <chrono>
+
+// inline int add_inline(int a, int b)
+// {
+//     return a + b;
+// }
+
+// int add_non_inline(int a, int b)
+// {
+//     return a + b;
+// }
+
+// int main()
+// {
+
+//     system("clear && printf '\e[3J'"); // clean the terminal before output in linux
+
+//     const int iterations = 100000000; // Large number of iterations to get noticeable time
+//     int result = 0;
+
+//     // Measure inline function time
+//     auto start_inline = std::chrono::high_resolution_clock::now();
+//     for (int i = 0; i < iterations; ++i)
+//     {
+//         result += add_inline(i, i + 1);
+//     }
+//     auto end_inline = std::chrono::high_resolution_clock::now();
+//     std::chrono::duration<double, std::nano> duration_inline = end_inline - start_inline;
+//     std::cout << "Inline function time: " << duration_inline.count() / iterations << " ns per call\n";
+
+//     result = 0; // Reset result
+
+//     // Measure non-inline function time
+//     auto start_non_inline = std::chrono::high_resolution_clock::now();
+//     for (int i = 0; i < iterations; ++i)
+//     {
+//         result += add_non_inline(i, i + 1);
+//     }
+//     auto end_non_inline = std::chrono::high_resolution_clock::now();
+//     std::chrono::duration<double, std::nano> duration_non_inline = end_non_inline - start_non_inline;
+//     std::cout << "Non-inline function time: " << duration_non_inline.count() / iterations << " ns per call\n";
+
+//     return 0;
+// }
+
+//-----------------------------------------------------------------------------
+// #include <iostream>
+// #include <chrono>
+
+// inline int add_inline(int a, int b)
+// {
+//     return a + b;
+// }
+
+// int add_non_inline(int a, int b)
+// {
+//     return a + b;
+// }
+
+// int main()
+// {
+
+//     system("clear && printf '\e[3J'"); // clean the terminal before output in linux
+
+//     const int iterations = 100000000;
+//     int result = 0;
+
+//     // Measure inline function time
+//     auto start_inline = std::chrono::high_resolution_clock::now();
+
+//     for (int i = 0; i < iterations; ++i)
+//     {
+//         result += add_inline(i, i + 1);
+//     }
+
+//     auto end_inline = std::chrono::high_resolution_clock::now();
+
+//     std::chrono::duration<double> duration_inline = end_inline - start_inline;
+
+//     std::cout << "Inline function time: " << duration_inline.count() << " seconds\n";
+
+//     result = 0; // Reset result
+
+//     // Measure non-inline function time
+//     auto start_non_inline = std::chrono::high_resolution_clock::now();
+
+//     for (int i = 0; i < iterations; ++i)
+//     {
+//         result += add_non_inline(i, i + 1);
+//     }
+
+//     auto end_non_inline = std::chrono::high_resolution_clock::now();
+
+//     std::chrono::duration<double> duration_non_inline = end_non_inline - start_non_inline;
+
+//     std::cout << "Non-inline function time: " << duration_non_inline.count() << " seconds\n";
+
+//     return 0;
+// }
+
+//---------------------------------------------------------------------------------------------------------
 #include <iostream>
 
 inline int square1(int x)
@@ -44,6 +146,11 @@ int main()
 //     return x + y;
 // }
 
+//  int add2(int x, int y)
+// {
+//     return x + y;
+// }
+
 // // Non-inline function
 // int multiply(int x, int y)
 // {
@@ -60,25 +167,35 @@ int main()
 
 //     // Measure time for inline function
 //     auto start_inline = std::chrono::high_resolution_clock::now();
+
 //     int inline_result = 0;
 //     for (int i = 0; i < iterations; ++i)
 //     {
 //         inline_result = add(a, b); // Call inline function
 //     }
+
 //     auto end_inline = std::chrono::high_resolution_clock::now();
-//     auto inline_duration = std::chrono::duration_cast<std::chrono::microseconds>(end_inline - start_inline);
+
+//     // auto inline_duration = std::chrono::duration_cast<std::chrono::microseconds>(end_inline - start_inline);
+//      auto inline_duration = std::chrono::duration_cast<std::chrono::seconds>(end_inline - start_inline);
+
 //     std::cout << "Inline function result: " << inline_result << "\n";
 //     std::cout << "Inline function time: " << inline_duration.count() << " microseconds\n";
 
 //     // Measure time for non-inline function
 //     auto start_non_inline = std::chrono::high_resolution_clock::now();
+
 //     int non_inline_result = 0;
 //     for (int i = 0; i < iterations; ++i)
 //     {
-//         non_inline_result = multiply(a, b); // Call non-inline function
+//          non_inline_result = multiply(a, b); // Call non-inline function
+//         //non_inline_result = add2(a, b); // Call non-inline function
 //     }
 //     auto end_non_inline = std::chrono::high_resolution_clock::now();
-//     auto non_inline_duration = std::chrono::duration_cast<std::chrono::microseconds>(end_non_inline - start_non_inline);
+
+//     // auto non_inline_duration = std::chrono::duration_cast<std::chrono::microseconds>(end_non_inline - start_non_inline);
+//     auto non_inline_duration = std::chrono::duration_cast<std::chrono::seconds>(end_non_inline - start_non_inline);
+
 //     std::cout << "Non-inline function result: " << non_inline_result << "\n";
 //     std::cout << "Non-inline function time: " << non_inline_duration.count() << " microseconds\n";
 
