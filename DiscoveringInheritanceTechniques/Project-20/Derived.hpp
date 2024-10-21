426ac28f0312d11790bb1887b0ffbee29de41a00
shiv <yadav.shivkumar5@gmail.com>
2024-10-21 23:42:25 +0530

    Inheritance

--- DiscoveringInheritanceTechniques/Project-20/Derived.hpp
+++ DiscoveringInheritanceTechniques/Project-20/Derived.hpp
@@ -7,5 +7,6 @@ class Derived : public Base
 public:
     using Base::someMethod;         // Explicitly "inherits" the Base version
     virtual void someMethod(int i); // Compiles, but doesn't override
+                                    // Adds a new version of someMethod
     virtual void someOtherMethod();
 };
