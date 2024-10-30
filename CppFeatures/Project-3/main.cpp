fa40fddf5d968b4037232a1af35ea2f65387c3db
shiv <yadav.shivkumar5@gmail.com>
2024-10-30 23:38:37 +0530

    cc

--- CppFeatures/Project-3/main.cpp
+++ CppFeatures/Project-3/main.cpp
@@ -2,6 +2,20 @@
  * @file main.cpp
  * @author your name (you@domain.com)
  * @brief benchmark inline function vs normal function
+
+   g++ -O2 main.cpp -o main
+
+    -O2 enables a moderate level of optimization.
+
+    Common optimization levels include
+     -O0 (no optimization),
+     -O2 (optimized code),
+     -O3 (aggressive optimization)
+
+
+    3e-8 in decimal form = 0.00000003
+
+
  * @version 0.1
  * @date 2024-10-16
  *
@@ -9,6 +23,53 @@
  *
  */
 
+#include <iostream>
+#include <chrono>
+
+// Inline function
+inline int add_inline(int a, int b)
+{
+    return a + b;
+}
+
+// Non-inline function
+int add_non_inline(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    // system("clear && printf '\e[3J'"); // clean the terminal before output in linux
+    const int iterations = 100000000;
+    int result = 0;
+
+    // Measure time for inline function
+    auto start_inline = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < iterations; ++i)
+    {
+        result += add_inline(i, i + 1);
+    }
+    auto end_inline = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration_inline = end_inline - start_inline;
+    std::cout << "Inline function time: " << duration_inline.count() << " seconds\n";
+
+    result = 0; // Reset result
+
+    // Measure time for non-inline function
+    auto start_non_inline = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < iterations; ++i)
+    {
+        result += add_non_inline(i, i + 1);
+    }
+    auto end_non_inline = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration_non_inline = end_non_inline - start_non_inline;
+    std::cout << "Non-inline function time: " << duration_non_inline.count() << " seconds\n";
+
+    return 0;
+}
+
+//-------------------------------------------------------------------
 // #include <iostream>
 // #include <chrono>
 
@@ -111,29 +172,29 @@
 // }
 
 //---------------------------------------------------------------------------------------------------------
-#include <iostream>
+// #include <iostream>
 
-inline int square1(int x)
-{
-    return x * x;
-}
+// inline int square1(int x)
+// {
+//     return x * x;
+// }
 
-int square2(int x)
-{
-    return x * x;
-}
+// int square2(int x)
+// {
+//     return x * x;
+// }
 
-int main()
-{
-    system("clear && printf '\e[3J'"); // clean the terminal before output in linux
+// int main()
+// {
+//     system("clear && printf '\e[3J'"); // clean the terminal before output in linux
 
-    int num = 5;
+//     int num = 5;
 
-    std::cout << "Square of " << num << " is " << square1(num) << std::endl; // Inline function call
-    std::cout << "Square of " << num << " is " << square2(num) << std::endl; // Normal function call
+//    std::cout << "Square of " << num << " is " << square1(num) << std::endl; // Inline function call
+//     //std::cout << "Square of " << num << " is " << square2(num) << std::endl; // Normal function call
 
-    return 0;
-}
+//     return 0;
+// }
 
 //----------------------------------------------------------------------------------------------------------
 
