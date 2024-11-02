f92c7e14d5fcdfd8714c9e1e01848c76dbac3a1b
shiv <yadav.shivkumar5@gmail.com>
2024-11-02 21:11:16 +0530

    Inheritance

---
+++ DiscoveringInheritanceTechniques/Project-32/main.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief Run-Time Type Facilities
+ *            One of the primary values of the typeid operator is for logging and debugging purposes.
+ * @version 0.1
+ * @date 2024-09-23
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include <iostream>
+
+
+int main()
+{
+    system("clear && printf '\e[3J'"); // clean the terminal before output in linux
+
+
+    return 0;
+}
