250ab5fe5936edd5a13e339ec5c20e70537e378c
shiv <yadav.shivkumar5@gmail.com>
2024-11-06 00:33:03 +0530

    thread

--- MultithreadedProgrammingwithCpp/Project-1/main.cpp
+++ MultithreadedProgrammingwithCpp/Project-1/main.cpp
@@ -11,7 +11,7 @@
             Counter 1 has value 1
             Counter 1 has value 2
             ...
-            
+
             This can be fixed using synchronization methods.
 
             g++ main.cpp -o test -pthread && ./test && rm test
@@ -25,6 +25,7 @@
 
 #include <iostream>
 #include <thread>
+#include <functional>
 
 void counter(int id, int numIterations)
 {
@@ -38,11 +39,24 @@ int main() // thread
 {
     system("clear && printf '\e[3J'"); // clean the terminal before output in linux
 
-    std::thread t1(counter, 1, 6);
-    std::thread t2(counter, 2, 4);
+    // std::thread t1(counter, 1, 6);
+    // std::thread t2(counter, 2, 4);
 
-    t1.join();
-    t2.join();
+    /*
+      Thread function arguments are always copied into some internal storage for the thread.
+      Use std::ref() or cref() from the <functional> header to pass them by reference.
+    */
+    // std::thread t3(std::ref(counter), 1, 6);
+    // std::thread t4(std::ref(counter), 2, 4);
+    std::thread t5(std::cref(counter), 1, 6);
+    std::thread t6(std::cref(counter), 2, 4);
+
+    // t1.join();
+    // t2.join();
+    // t3.join();
+    // t4.join();
+    t5.join();
+    t6.join();
 
     return 1;
 }
