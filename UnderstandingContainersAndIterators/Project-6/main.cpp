83e715b0a47ce84390d36a44a34f7bfe93b37963
shiv <yadav.shivkumar5@gmail.com>
2024-11-27 23:12:51 +0530

    ContainersAndIterators

---
+++ UnderstandingContainersAndIterators/Project-6/main.cpp
@@ -0,0 +1,44 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief Copying and Assigning vectors
+ *
+ * @version 0.1
+ * @date 2024-09-23
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    system("clear && printf '\e[3J'"); // clean the terminal before output in linux
+
+    std::vector<int> vectorOne(10);
+    std::vector<int> vectorTwo(10);
+
+    if (vectorOne == vectorTwo)
+    {
+        std::cout << "equal!" << std::endl;
+    }
+    else
+    {
+        std::cout << "not equal!" << std::endl;
+    }
+
+    vectorOne[3] = 50;
+
+    if (vectorOne < vectorTwo)
+    {
+        std::cout << "vectorOne is less than vectorTwo" << std::endl;
+    }
+    else
+    {
+        std::cout << "vectorOne is not less than vectorTwo" << std::endl;
+    }
+
+    return 0;
+}
