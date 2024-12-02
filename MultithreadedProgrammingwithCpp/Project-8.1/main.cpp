1ad06343424eddad7acef6eba317a34a2e1cc711
shiv <yadav.shivkumar5@gmail.com>
2024-12-02 23:56:11 +0530

    thread

---
+++ MultithreadedProgrammingwithCpp/Project-8.1/main.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief
+
+        MUTUAL EXCLUSION
+        g++ main.cpp -o test -pthread && ./test && rm test
+
+
+ * @version 0.1
+ * @date 2024-10-21
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+int main()
+{
+    system("clear && printf '\e[3J'"); // clean the terminal before output in linux
+
+    return 0;
+}
