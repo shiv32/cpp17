6c80fe45fd1f096547158fa6f9f11b166e7531fd
shiv <yadav.shivkumar5@gmail.com>
2024-12-03 20:03:38 +0530

    AlgoTradeCpp

---
+++ AlgoTradingCpp/Project-2/main.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief  Algorithmic trading
+
+
+ * @version 0.1
+ * @date 2024-10-05
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include <iostream>
+
+
+int main()
+{
+    system("clear && printf '\e[3J'"); // clean the terminal before output in linux
+   
+    return 0;
+}
