8ef97d5cad2d6cd2be9594da9cbadfe561ca2e09
shiv <yadav.shivkumar5@gmail.com>
2024-12-09 00:06:10 +0530

    SPV

---
+++ SimpleProcessViewerCpp/main.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief SPV
+
+ * @version 0.1
+ * @date 2024-10-05
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include <iostream>
+
+
+int main()
+{
+
+    system("clear && printf '\e[3J'"); // clean the terminal before output in linux
+
+
+
+    return 0;
+}
