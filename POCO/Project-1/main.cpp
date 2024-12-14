d758eb8b2da061215250359402767eb02d289341
shiv <yadav.shivkumar5@gmail.com>
2024-12-14 12:52:06 +0530

    poco cpp

---
+++ POCO/Project-1/main.cpp
@@ -0,0 +1,51 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief A simple program to test the C++ POCO installation: HTTP Client
+ *
+ *  g++ -o http_client main.cpp -lPocoFoundation -lPocoNet -lPocoUtil
+
+    ./http_client
+
+    g++ -o http_client main.cpp -lPocoFoundation -lPocoNet -lPocoUtil && ./http_client && rm http_client
+
+ * @version 0.1
+ * @date 2024-12-14
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include <Poco/Net/HTTPClientSession.h>
+#include <Poco/Net/HTTPRequest.h>
+#include <Poco/Net/HTTPResponse.h>
+#include <Poco/StreamCopier.h>
+#include <iostream>
+#include <sstream>
+
+using namespace Poco::Net;
+using namespace Poco;
+using namespace std;
+
+int main()
+{
+    try
+    {
+        HTTPClientSession session("www.google.com");
+        HTTPRequest request(HTTPRequest::HTTP_GET, "/");
+        session.sendRequest(request);
+
+        HTTPResponse response;
+        istream &responseStream = session.receiveResponse(response);
+
+        cout << "Response: " << response.getStatus() << " " << response.getReason() << endl;
+
+        // StreamCopier::copyStream(responseStream, cout);
+    }
+    catch (const Exception &ex)
+    {
+        cerr << "Exception: " << ex.displayText() << endl;
+    }
+
+    return 0;
+}
