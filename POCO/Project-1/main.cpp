51d6a326baf5a8d8f6a3765e2fdb590491f09187
shiv <yadav.shivkumar5@gmail.com>
2024-12-15 14:15:12 +0530

    poco

--- POCO/Project-1/main.cpp
+++ POCO/Project-1/main.cpp
@@ -2,7 +2,7 @@
  * @file main.cpp
  * @author your name (you@domain.com)
  * @brief A simple program to test the C++ POCO installation: HTTP Client
- 
+
     g++ -o http_client main.cpp -lPocoFoundation -lPocoNet -lPocoUtil && ./http_client && rm http_client
 
  * @version 0.1
@@ -12,6 +12,7 @@
  *
  */
 
+#include <Poco/Version.h>
 #include <Poco/Net/HTTPClientSession.h>
 #include <Poco/Net/HTTPRequest.h>
 #include <Poco/Net/HTTPResponse.h>
@@ -25,16 +26,28 @@ using namespace std;
 
 int main()
 {
+    system("clear && printf '\e[3J'"); // clean the terminal before output in linux
+
     try
     {
-        HTTPClientSession session("www.google.com");
+        // Decode the hex version
+        int major = (POCO_VERSION >> 24) & 0xFF; // Extract major version
+        int minor = (POCO_VERSION >> 16) & 0xFF; // Extract minor version
+        int patch = (POCO_VERSION) & 0xFFFF;     // Extract patch version
+
+        std::string website = "www.google.com";
+
+        HTTPClientSession session(website);
         HTTPRequest request(HTTPRequest::HTTP_GET, "/");
         session.sendRequest(request);
 
         HTTPResponse response;
         istream &responseStream = session.receiveResponse(response);
 
-        cout << "Response: " << response.getStatus() << " " << response.getReason() << endl;
+        std::cout << "Poco version: "
+                  << major << "." << minor << "." << patch << std::endl;
+
+        cout << website << " " << "Response: " << response.getStatus() << " " << response.getReason() << endl;
 
         // StreamCopier::copyStream(responseStream, cout);
     }
