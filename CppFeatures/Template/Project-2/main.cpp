073526eabcf6ad7543f16877fb46d30c9f9438b0
shiv <yadav.shivkumar5@gmail.com>
2024-12-18 01:54:34 +0530

    templates

--- CppFeatures/Template/Project-2/main.cpp
+++ CppFeatures/Template/Project-2/main.cpp
@@ -48,15 +48,31 @@ class Stack
     int m_Top{-1};
 
 public:
+    Stack() = default;
+
+    Stack(const Stack &obj);
+
+    // Stack(const Stack &obj)
+    // Stack(const Stack<T, size> &obj)
+    // {
+    //     m_Top = obj.m_Top;
+
+    //     for (int i = 0; i <= m_Top; ++i)
+    //     {
+    //         m_Buffer[i] = obj.m_Buffer[i];
+    //     }
+    // }
+
     void Push(const T &elem)
     {
         m_Buffer[++m_Top] = elem;
     }
 
-    void Pop()
-    {
-        --m_Top;
-    }
+    void Pop();
+    // void Pop()
+    // {
+    //     --m_Top;
+    // }
 
     const T &Top() const
     {
@@ -67,7 +83,38 @@ public:
     {
         return m_Top == -1;
     }
+
+    // static Stack Create() //factory
+    // {
+    //     return Stack<T, size>();
+    // }
+
+    static Stack Create(); // factory
 };
+
+template <typename T, int size>
+void Stack<T, size>::Pop()
+{
+    --m_Top;
+}
+
+template <typename T, int size>
+Stack<T, size> Stack<T, size>::Create() // factory
+{
+    return Stack<T, size>();
+}
+
+template <typename T, int size>
+// Stack<T, size>::Stack(const Stack &obj)   //working
+Stack<T, size>::Stack(const Stack<T, size> &obj)
+{
+    m_Top = obj.m_Top;
+
+    for (int i = 0; i <= m_Top; ++i)
+    {
+        m_Buffer[i] = obj.m_Buffer[i];
+    }
+}
 
 int main()
 {
@@ -90,17 +137,30 @@ int main()
     */
 
     // Stack<int,10> s;
-    Stack<float, 10> s;
+    // Stack<float, 10> s;
+    Stack<float, 10> s = Stack<float, 10>::Create();
 
     s.Push(3);
     s.Push(1);
     s.Push(6);
     s.Push(9);
 
-    while (!s.IsEmpty())
+    // std::cout << "s: " << std::endl;
+
+    // while (!s.IsEmpty())
+    // {
+    //     std::cout << s.Top() << std::endl;
+    //     s.Pop();
+    // }
+
+    auto s2(s);
+
+    std::cout << "s2: " << std::endl;
+
+    while (!s2.IsEmpty())
     {
-        std::cout << s.Top() << std::endl;
-        s.Pop();
+        std::cout << s2.Top() << std::endl;
+        s2.Pop();
     }
 
     return 0;
