a016953cc3af65e7a8e7f2d6da47091b1db8867e
shiv <yadav.shivkumar5@gmail.com>
2025-02-27 01:01:42 +0530

    thread

--- MultithreadedProgrammingwithCpp/Project-14/main.cpp
+++ MultithreadedProgrammingwithCpp/Project-14/main.cpp
@@ -1,5 +1,17 @@
-// std::promise and std::future
-// g++ --std=c++17 main.cpp -o test -pthread
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * 
+        std::promise and std::future
+        g++ --std=c++17 main.cpp -o test -pthread
+ * 
+ * @version 0.1
+ * @date 2025-02-27
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
 
 #include <iostream>
 #include <thread>
@@ -11,8 +23,11 @@ void DoWork(std::promise<int> thePromise)
     // And ultimately store the result in the promise.
     thePromise.set_value(42);
 }
+
 int main()
 {
+    system("clear && printf '\e[3J'"); // clean the terminal before output in linux
+
     // Create a promise to pass to the thread.
     std::promise<int> myPromise;
 
@@ -24,7 +39,9 @@ int main()
 
     // Do some more work...
     // Get the result.
-    int result = theFuture.get();
+    // int result = theFuture.get();
+    auto result = theFuture.get();
+    
     std::cout << "Result: " << result << std::endl;
 
     // Make sure to join the thread.
