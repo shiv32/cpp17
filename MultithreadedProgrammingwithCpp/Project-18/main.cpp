3b6432f2cf69b2a4ee51cdb4bd82daab01f80402
shiv <yadav.shivkumar5@gmail.com>
2025-03-01 00:52:12 +0530

    thread

--- MultithreadedProgrammingwithCpp/Project-18/main.cpp
+++ MultithreadedProgrammingwithCpp/Project-18/main.cpp
@@ -1,5 +1,17 @@
-// Exception Handling std::async
-// g++ --std=c++17 main.cpp -o test -pthread
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief
+ *
+    Exception Handling std::async
+    g++ --std=c++17 main.cpp -o test -pthread
+ *
+ * @version 0.1
+ * @date 2025-03-01
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
 
 #include <iostream>
 #include <future>
@@ -11,9 +23,13 @@ int calculate()
 
 int main()
 {
+    system("clear && printf '\e[3J'"); // clean the terminal before output in linux
+
     // Use the launch::async policy to force asynchronous execution.
     auto myFuture = std::async(std::launch::async, calculate);
+
     // Do some more work...
+
     // Get the result.
     try
     {
