2136cf819ec17fcfdafb482ae88dfc6e5b1442d4
shiv <yadav.shivkumar5@gmail.com>
2025-03-12 20:11:11 +0530

    avr cpp

--- Microcontroller/Project-18/main.cpp
+++ Microcontroller/Project-18/main.cpp
@@ -3,7 +3,7 @@
  * @author your name (you@domain.com)
  * @brief
  *
- * Tested on simulIDE
+ * Tested on simulIDE & simavr
  *
  * avr-g++ -mmcu=atmega328p -std=c++17 -Os -o main.elf main.cpp && avr-objcopy -O ihex main.elf main.hex
  *
