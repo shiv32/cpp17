2630f8d3be1d57156af29a4f890eb86651730a9f
shiv <yadav.shivkumar5@gmail.com>
2025-03-13 01:49:59 +0530

    Templates

--- WritingGenericCodewithTemplates/Project-1/main.cpp
+++ WritingGenericCodewithTemplates/Project-1/main.cpp
@@ -1,24 +1,36 @@
-/*
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * 
+    Basic template demo
 
-Basic template demo
+    g++ main.cpp -o test
+ * 
+ * 
+ * @version 0.1
+ * @date 2025-03-13
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
 
-g++ main.cpp -o test
-
-*/
 
 #include <iostream>
 
 template <typename T>
-T findMax(T a,T b)
+T findMax(T a, T b)
 {
-   return (a>b)?a:b;
+    return (a > b) ? a : b;
 }
 
 int main()
 {
-    std::cout<<findMax(2,4)<<std::endl;
+    system("clear && printf '\e[3J'"); // clean the terminal before output in linux
 
-    std::cout<<findMax(4.6f,8.3f)<<std::endl;
+    std::cout << findMax(2, 4) << std::endl;
+
+    std::cout << findMax(4.6f, 8.3f) << std::endl;
 
     return 0;
 }
