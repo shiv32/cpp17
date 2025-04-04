9014be6ffe50ab7cb2b6fc7489a06288b150b7d9
shiv <yadav.shivkumar5@gmail.com>
2025-04-05 00:33:40 +0530

    Templates

--- WritingGenericCodewithTemplates/Project-7/main.cpp
+++ WritingGenericCodewithTemplates/Project-7/main.cpp
@@ -1,16 +1,27 @@
-/*
-Template Definitions in Header Files
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief
+ *
+    Template Definitions in Header Files
 
-g++ -std=c++17 *.cpp -o test
-*/
+    g++ -std=c++17 *.cpp -o test
+ *
+ * @version 0.1
+ * @date 2025-04-05
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
 
 #include <iostream>
 #include "grid.hpp"
 
 int main()
 {
+    system("clear && printf '\e[3J'"); // clean the terminal before output in linux
 
-    Grid<int> myIntGrid; 
+    Grid<int> myIntGrid;
     myIntGrid.at(0, 0) = 10;
 
     return 0;
