7c95ce86f6b427b1442b9976ff6c482bbbf77aa9
shiv <yadav.shivkumar5@gmail.com>
2025-05-17 18:40:01 +0530

    Templates

--- WritingGenericCodewithTemplates/Project-15/grid.cpp
+++ WritingGenericCodewithTemplates/Project-15/grid.cpp
@@ -10,7 +10,7 @@ void Grid<T, WIDTH, HEIGHT>::verifyCoordinate(size_t x, size_t y) const
 {
     if (x >= WIDTH || y >= HEIGHT)
     {
-        throw std::out_of_range("");
+        throw std::out_of_range("Out of range !");
     }
 }
 
@@ -31,10 +31,10 @@ template <typename T, size_t WIDTH, size_t HEIGHT>
 template <typename E, size_t WIDTH2, size_t HEIGHT2>
 Grid<T, WIDTH, HEIGHT>::Grid(const Grid<E, WIDTH2, HEIGHT2> &src)
 {
-    std::cout << __PRETTY_FUNCTION__ << std::endl;
-    std::cout << "Template type : " << typeid(E).name() << std::endl;
-    std::cout << "WIDTH2 : " << WIDTH2 << std::endl;
-    std::cout << "HEIGHT2 : " << HEIGHT2 << std::endl;
+    // std::cout << __PRETTY_FUNCTION__ << std::endl;
+    // std::cout << "Template type : " << typeid(E).name() << std::endl;
+    // std::cout << "WIDTH2 : " << WIDTH2 << std::endl;
+    // std::cout << "HEIGHT2 : " << HEIGHT2 << std::endl;
 
     for (size_t i = 0; i < WIDTH; i++)
     {
@@ -63,10 +63,10 @@ template <typename T, size_t WIDTH, size_t HEIGHT>
 template <typename E, size_t WIDTH2, size_t HEIGHT2>
 Grid<T, WIDTH, HEIGHT> &Grid<T, WIDTH, HEIGHT>::operator=(const Grid<E, WIDTH2, HEIGHT2> &rhs)
 {
-    std::cout << __PRETTY_FUNCTION__ << std::endl;
-    std::cout << "Template type : " << typeid(E).name() << std::endl;
-    std::cout << "WIDTH2 : " << WIDTH2 << std::endl;
-    std::cout << "HEIGHT2 : " << HEIGHT2 << std::endl;
+    // std::cout << __PRETTY_FUNCTION__ << std::endl;
+    // std::cout << "Template type : " << typeid(E).name() << std::endl;
+    // std::cout << "WIDTH2 : " << WIDTH2 << std::endl;
+    // std::cout << "HEIGHT2 : " << HEIGHT2 << std::endl;
 
     // no need to check for self-assignment because this version of
     // assignment is never called when T and E are the same
--- WritingGenericCodewithTemplates/Project-15/main.cpp
+++ WritingGenericCodewithTemplates/Project-15/main.cpp
@@ -9,19 +9,32 @@ g++ -std=c++17 main.cpp -o test
 
 int main()
 {
+    system("clear && printf '\e[3J'"); // clean the terminal before output in linux
+
     Grid<int, 15, 15> intGrid;
-
     intGrid.at(5, 5) = 67;
-
     std::cout << intGrid.at(5, 5).value_or(0) << std::endl;
 
     Grid<double, 10, 10> doubleGrid(intGrid);
-
     std::cout << doubleGrid.at(5, 5).value_or(0) << std::endl;
 
     doubleGrid = intGrid;
-
     std::cout << doubleGrid.at(5, 5).value_or(0) << std::endl;
+
+    //-------------------------
+    try
+    {
+        Grid<int, 4, 4> intGrid2;
+        intGrid2.at(3, 3) = 68;
+
+        Grid<double, 15, 15> doubleGrid2(intGrid2);
+        std::cout << doubleGrid2.at(3, 3).value_or(0) << std::endl;
+        std::cout << doubleGrid2.at(5, 5).value_or(0) << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "exception : " << e.what() << '\n';
+    }
 
     return 0;
 }
