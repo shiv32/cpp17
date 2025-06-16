e4971b5f604a73c375686b8ba9c543af161621b6
shiv <yadav.shivkumar5@gmail.com>
2025-06-17 00:46:10 +0530

    Templates

--- WritingGenericCodewithTemplates/Project-24/main.cpp
+++ WritingGenericCodewithTemplates/Project-24/main.cpp
@@ -1,31 +1,23 @@
-/*
-More on Template Parameter Deduction
-
-g++ -std=c++17 main.cpp -o test
-*/
-
 #include <iostream>
 #include "mytemplate.hpp"
 
 int main()
 {
+    system("clear && printf '\e[3J'"); // clean the terminal before output in linux
+
     // case 1
     auto result = add<long long, int, int>(1, 2);
-
     std::cout << "result : " << result << std::endl;
 
-    result = add<long long>(1, 2);
-
+    result = add<long long>(1, 3);
     std::cout << "result : " << result << std::endl;
 
     // case 2
-    result = add2<int, long long>(1, 5);
-
+    result = add<int, long long>(1, 5);
     std::cout << "result : " << result << std::endl;
 
     // case 3
-    result = add3(1, 6);
-
+    result = add2(1, 6);
     std::cout << "result : " << result << std::endl;
 
     return 0;
--- WritingGenericCodewithTemplates/Project-24/mytemplate.hpp
+++ WritingGenericCodewithTemplates/Project-24/mytemplate.hpp
@@ -9,7 +9,7 @@ RetType add(const T1 &t1, const T2 &t2)
 
 // case 2
 template <typename T1, typename RetType, typename T2>
-RetType add2(const T1 &t1, const T2 &t2)
+RetType add(const T1 &t1, const T2 &t2)
 {
     std::cout << "---------- case 2 ------------" << std::endl;
     return t1 + t2;
@@ -17,7 +17,7 @@ RetType add2(const T1 &t1, const T2 &t2)
 
 // case 3
 template <typename RetType = long long, typename T1, typename T2>
-RetType add3(const T1 &t1, const T2 &t2)
+RetType add2(const T1 &t1, const T2 &t2)
 {
     std::cout << "---------- case 3 ------------" << std::endl;
     return t1 + t2;
