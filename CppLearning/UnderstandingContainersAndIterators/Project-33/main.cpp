99f93ba2ccdd745a04cef58c1e2d42120b54c2a9
shiv32 <yadav.shivkumar5@gmail.com>
2025-07-18 02:51:44 +0530

    ContainersAndIterators

---
+++ CppLearning/UnderstandingContainersAndIterators/Project-33/main.cpp
@@ -0,0 +1,61 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief map Removing Elements
+ * @version 0.1
+ * @date 2025-07-03
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+
+#include <iostream>
+#include <map>
+
+class Data final
+{
+public:
+    explicit Data(int value = 0) : mValue(value) {}
+    int getValue() const
+    {
+        return mValue;
+    }
+    void setValue(int value)
+    {
+        mValue = value;
+    }
+
+private:
+    int mValue;
+};
+
+void printMap(const std::map<int, Data> &dataMap)
+{
+    // Print keys and values
+    // for (const auto &pair : dataMap)
+    // {
+    //     std::cout << "Key: " << pair.first << ", Value: " << pair.second.getValue() << std::endl;
+    // }
+
+    // combination of a range-based for loop and C++17 structured bindings
+    for (const auto &[key, data] : dataMap)
+    {
+        std::cout << "Key: " << key << ", Value: " << data.getValue() << std::endl;
+    }
+}
+
+int main()
+{
+    system("clear && printf '\e[3J'"); // clean the terminal before output in linux
+
+    std::map<int, Data> dataMap;
+
+    dataMap[1] = Data(4);
+    printMap(dataMap);
+
+    std::cout << "There are " << dataMap.count(1) << " elements with key 1" << std::endl;
+    dataMap.erase(1);
+    std::cout << "There are " << dataMap.count(1) << " elements with key 1" << std::endl;
+
+    return 0;
+}
