8544fc722a887d32825dbb8a519e8080ceb87f44
shiv32 <yadav.shivkumar5@gmail.com>
2025-07-20 23:45:55 +0530

    cppInterview

--- Interviews/Project-1/main.cpp
+++ Interviews/Project-1/main.cpp
@@ -17,13 +17,13 @@ struct A
     }
 
     // 3. Copy constructor
-    A(const A& other) : x(other.x)
+    A(const A &other) : x(other.x)
     {
         std::cout << "Copy: " << x << "\n";
     }
 
     // 4. Move constructor
-    A(A&& other) noexcept : x(other.x)
+    A(A &&other) noexcept : x(other.x)
     {
         other.x = 0;
         std::cout << "Move\n";
@@ -42,8 +42,9 @@ struct A
     }
 
     // 7. Bool constructor (manual instead of defaulted)
-    //A(bool b)
+
     explicit A(bool b)
+    // A(bool b)
     {
         x = b ? 1 : 0;
         std::cout << "Bool ctor: " << x << "\n";
@@ -51,7 +52,7 @@ struct A
 
     // 8. Deleted constructor
     A(std::string) = delete;
-    A(const char*) = delete;  // Prevent using string literals
+    // A(const char*) = delete;  // Prevent using string literals
 
     // 9. constexpr constructor
     constexpr A(float f) : x(static_cast<int>(f)) {}
@@ -73,22 +74,21 @@ int main()
 {
     system("clear && printf '\e[3J'"); // clean the terminal before output in linux
 
-     A a1;               // Default
+    // A a1;               // Default
     // A a2(10);           // Parameterized
     // A a3 = a2;          // Copy
     // A a4 = std::move(a2); // Move
     // A a5 = A(5.5);      // Delegating
     // A a6 = A('A');      // Explicit
 
-    //A a7("test");    // Deleted - compile error if uncommented
-    //A a7("test");    //allowed: direct initialization, call  A(bool b)
-    //A a7 = "test";   //error: copy initialization, not allowed with `explicit`
+    // A a7("test");    // Deleted - compile error if uncommented
+    // A a7 = "test";   //error: copy initialization, not allowed with `explicit` or deleted constructors
 
-    //A a8 = A(true);         // Bool
+    // A a8 = A(true); // Bool
 
     //  constexpr A a9(3.14f); // constexpr constructor
     //  a9.show();
 
-    // B b1{1, 2};         // Aggregate initialization
-    // std::cout << "Aggregate: " << b1.x << ", " << b1.y << "\n";
+    B b1{1, 2}; // Aggregate initialization
+    std::cout << "Aggregate: " << b1.x << ", " << b1.y << "\n";
 }
