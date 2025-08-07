e04922e8b591ef4faa65abb25c49961c8c3432dc
shiv32 <yadav.shivkumar5@gmail.com>
2025-08-07 23:20:26 +0530

    cc

--- DSA/CppPointers&Applications/Introduction/Project-2/main.cpp
+++ DSA/CppPointers&Applications/Introduction/Project-2/main.cpp
@@ -2,32 +2,6 @@
  * @file main.cpp
  * @author your name (you@domain.com)
  * @brief Introduction to Pointers & Examples of Pointers
- * 
-                (Process)
-
-                ------------------------------------------------------------
-                    0x7fffffffd75c  <- high memroy address
-
-
-
-                    
-                ------------------------------------------------------------
-                ⬇   stack           <- local variables
-                ------------------------------------------------------------
-
-
-                ------------------------------------------------------------
-                ⬆   heap            <- dynamically allocated memory
-                ------------------------------------------------------------
-                    data            <- global and static variables
-                ------------------------------------------------------------    
-                    code            <- programs executable code
-                -------------------------------------------------------------
-                    0x00000000      <- low memroy address
-                -------------------------------------------------------------
-
-            (virtual address space)
-
  * @version 0.1
  * @date 2024-08-31
  * 
