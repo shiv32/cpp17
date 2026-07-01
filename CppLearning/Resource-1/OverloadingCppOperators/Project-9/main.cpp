0aa7ddc2ae70c926c344be4857bfc815605da2e5
shiv32 <yadav.shivkumar5@gmail.com>
2026-07-02 01:17:18 +0530

    opov

--- CppLearning/Resource-1/OverloadingCppOperators/Project-9/main.cpp
+++ CppLearning/Resource-1/OverloadingCppOperators/Project-9/main.cpp
@@ -27,6 +27,7 @@ namespace example2
 
             void* operator new(size_t size);
             void* operator new[](size_t size);
+            
             void* operator new(size_t size, const std::nothrow_t&) noexcept;
             void* operator new[](size_t size, const std::nothrow_t&) noexcept;
 
@@ -50,8 +51,10 @@ namespace example2
 
             void operator delete(void* ptr) noexcept;
             void operator delete[](void* ptr) noexcept;
+
             void operator delete(void* ptr, const std::nothrow_t&) noexcept;
             void operator delete[](void* ptr, const std::nothrow_t&) noexcept;
+
             void operator delete(void* p, void*) noexcept;
             void operator delete[](void* p, void*) noexcept;
         */
