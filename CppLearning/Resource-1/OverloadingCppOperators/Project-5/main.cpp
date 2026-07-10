8662eb2cc78c83014c3ac7bb0044c720b68eb635
shiv32 <yadav.shivkumar5@gmail.com>
2026-07-11 02:09:52 +0530

    refactor

--- CppLearning/Resource-1/OverloadingCppOperators/Project-5/main.cpp
+++ CppLearning/Resource-1/OverloadingCppOperators/Project-5/main.cpp
@@ -4,88 +4,91 @@
 
 using namespace std;
 
-void printArray(const Array<int> &arr)
+namespace example1
 {
-    // cout<<arr.getSize()<<"\n";
-
-    for (size_t i = 0; i < arr.getSize(); i++)
+    void printArray(const Array<int> &arr)
     {
-        cout << arr[i] << " "; // Calls the const operator[] because arr is a const object.
+        // cout<<arr.getSize()<<"\n";
+
+        for (size_t i = 0; i < arr.getSize(); i++)
+        {
+            cout << arr[i] << " "; // Calls the const operator[] because arr is a const object.
+        }
+
+        // cout << arr[100] << " ";
+
+        cout << endl;
     }
 
-    // cout << arr[100] << " ";
-
-    cout << endl;
-}
-
-void test1()
-{
-    Array<int> myArray;
-
-    for (size_t i = 0; i < 10; i++)
+    void test1()
     {
-        myArray.setElementAt(i, 100);
+        Array<int> myArray;
+
+        for (size_t i = 0; i < 10; i++)
+        {
+            myArray.setElementAt(i, 100);
+        }
+
+        for (size_t i = 0; i < 10; i++)
+        {
+            cout << myArray.getElementAt(i) << " ";
+        }
+
+        cout << "\n";
     }
 
-    for (size_t i = 0; i < 10; i++)
+    void test2()
     {
-        cout << myArray.getElementAt(i) << " ";
+        Array<int> myArray;
+
+        for (size_t i = 0; i < 10; i++)
+        {
+            myArray[i] = 100;
+        }
+
+        for (size_t i = 0; i < 10; i++)
+        {
+            cout << myArray[i] << " ";
+        }
+
+        cout << "\n";
     }
 
-    cout << "\n";
-}
-
-void test2()
-{
-    Array<int> myArray;
-
-    for (size_t i = 0; i < 10; i++)
+    void test3()
     {
-        myArray[i] = 100;
+        Array<int> myArray;
+
+        for (size_t i = 0; i < 10; i++)
+        {
+            myArray[i] = 100; // Calls the non-const operator[] because myArray is a non-const object.
+        }
+
+        printArray(myArray);
     }
 
-    for (size_t i = 0; i < 10; i++)
+    void test4()
     {
-        cout << myArray[i] << " ";
-    }
+        AssociativeArray<int> myArray;
 
-    cout << "\n";
-}
+        myArray["Key 1"] = 11;
+        myArray["Key 2"] = 22;
+        myArray["Key 3"] = 33;
 
-void test3()
-{
-    Array<int> myArray;
-    
-    for (size_t i = 0; i < 10; i++)
-    {
-        myArray[i] = 100; // Calls the non-const operator[] because myArray is a non-const object.
-    }
+        try
+        {
+            cout << myArray["Key 1"] << endl;
+            cout << myArray["Key 2"] << endl;
 
-    printArray(myArray);
-}
+            // Test const operator[]
+            const AssociativeArray<int> &c = myArray;
 
-void test4()
-{
-    AssociativeArray<int> myArray;
-    
-    myArray["Key 1"] = 11;
-    myArray["Key 2"] = 22;
-    myArray["Key 3"] = 33;
-
-    try
-    {
-        cout << myArray["Key 1"] << endl;
-        cout << myArray["Key 2"] << endl;
-
-        // Test const operator[]
-        const AssociativeArray<int> &c = myArray;
-
-        cout << c["Key 3"] << endl;
-        cout << c["Key 4"] << endl;
-    }
-    catch (const invalid_argument &ex)
-    {
-        cout << "Caught exception: " << ex.what() << endl;
+            cout << c["Key 3"] << endl;
+            cout << c["Key 4"] << endl;
+        }
+        catch (const invalid_argument &ex)
+        {
+            cout << "Caught exception: " << ex.what() << endl;
+        }
     }
 }
 
@@ -93,10 +96,10 @@ int main()
 {
     system("clear && printf '\e[3J'"); // clean the terminal before output in linux
 
-    // test1();
-    // test2();
-    // test3();
-    test4();
+    // example1::test1();
+    // example1::test2();
+    // example1::test3();
+    example1::test4();
 
     return 0;
 }
