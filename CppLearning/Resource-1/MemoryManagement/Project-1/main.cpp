b70b1206d5650973320909bd07849e9de071fdb8
shiv32 <yadav.shivkumar5@gmail.com>
2026-07-24 02:00:15 +0530

    mm

--- CppLearning/Resource-1/MemoryManagement/Project-1/main.cpp
+++ CppLearning/Resource-1/MemoryManagement/Project-1/main.cpp
@@ -1,48 +1,42 @@
 #include <iostream>
 
-int main() 
+using namespace std;
+
+namespace example1
+{
+    void test1()
+    {
+        int i = 7; // i is a so-called automatic variable allocated on the stack.
+
+        int *ptr = nullptr; // variable ptr on the stack initialized with nullptr
+        ptr = new int;      // When you use the new keyword, memory is allocated on the heap.
+
+        // above code in one-liner
+        int *ptr2 = new int; // When you use the new keyword, memory is allocated on the heap.
+
+        // pointers can exist both on the stack and on the heap
+        int **handle = nullptr;
+        handle = new int *; // stack (handle)
+        *handle = new int;  // heap (*handle)
+    }
+
+    void leaky()
+    {
+        new int; // BUG! Orphans/leaks memory!
+        cout << "I just leaked an int!" << endl;
+    }
+
+    void test2()
+    {
+        int *ptr = new int;
+        delete ptr;
+        ptr = nullptr;
+    }
+}
+
+int main()
 {
     system("clear && printf '\e[3J'"); // clean the terminal before output in linux
-
-    int value = 10;
-    int *ptr1 = &value;
-
-    std::cout << &value << "\n";
-    std::cout << value << "\n";
-
-    std::cout << &ptr1 << "\n";
-    std::cout << ptr1 << "\n";
-    std::cout << *ptr1 << "\n";
-
-    int **handlee = &ptr1;
-
-    std::cout << handlee << "\n";
-    std::cout << *handlee << "\n";
-    std::cout << **handlee << "\n";
-
-    std::cout << "-----------------------------------------\n";
-
-    int i = 7;
-
-    std::cout << &i << "\n";
-    std::cout << i << "\n";
-
-    int *ptr = nullptr; // int* ptr = new int;
-    // ptr = new int;
-    ptr = new int(45);
-
-    std::cout << &ptr << "\n";
-    std::cout << ptr << "\n";
-    std::cout << *ptr << "\n";
-
-    int **handle = nullptr;
-    handle = new int *;
-    *handle = new int(6);
-
-    std::cout << &handle << "\n";
-    std::cout << handle << "\n";
-    std::cout << *handle << "\n";
-    std::cout << **handle << "\n";
 
     return 1;
 }
